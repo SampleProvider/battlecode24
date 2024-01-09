a8b1524e8c619c23594f577547bdbc5a16ae060b
SampleProvider <68207888+SampleProvider@users.noreply.github.com>
2024-01-09 16:27:00 -0500

    Add basic defensive strategy and fix bugnav

--- src/Defensive_SPAARK/Defensive.java
+++ src/Defensive_SPAARK/Defensive.java
@@ -14,8 +14,37 @@ public class Defensive {
     public static StringBuilder indicatorString;
 
     public static Random rng;
+
+    protected static final Direction[] DIRECTIONS = {
+        Direction.SOUTHWEST,
+        Direction.SOUTH,
+        Direction.SOUTHEAST,
+        Direction.WEST,
+        Direction.EAST,
+        Direction.NORTHWEST,
+        Direction.NORTH,
+        Direction.NORTHEAST,
+    };
     
     public static void run() throws GameActionException {
-
+        FlagInfo[] flags = rc.senseNearbyFlags(-1, rc.getTeam());
+        if (flags.length > 0) {
+            Motion.bug2(flags[0].getLocation());
+        }
+        else {
+            Motion.moveRandomly();
+        }
+        MapLocation buildLoc = rc.getLocation().add(DIRECTIONS[rng.nextInt(8)]);
+        if (rc.canBuild(TrapType.EXPLOSIVE, buildLoc) && rng.nextInt() % 37 == 1) {
+            rc.build(TrapType.EXPLOSIVE, buildLoc);
+        }
+        else if (rc.canBuild(TrapType.WATER, buildLoc) && rng.nextInt() % 37 == 1) {
+            rc.build(TrapType.WATER, buildLoc);
+        }
+        else if (rc.canBuild(TrapType.STUN, buildLoc) && rng.nextInt() % 37 == 1) {
+            rc.build(TrapType.STUN, buildLoc);
+        }
+        Attack.attack();
+        Attack.heal();
     }
 }
--- src/Defensive_SPAARK/Offensive.java
+++ src/Defensive_SPAARK/Offensive.java
@@ -16,31 +16,6 @@ public class Offensive {
     public static Random rng;
     
     public static void run() throws GameActionException {
-        // capturing flags
-        if (rc.canPickupFlag(rc.getLocation())) {
-            rc.pickupFlag(rc.getLocation());
-        }
-        if (rc.hasFlag()) {
-            MapLocation[] spawnLocs = rc.getAllySpawnLocations();
-            MapLocation firstLoc = spawnLocs[0];
-            Motion.bug2(firstLoc);
-        }
-        else {
-            FlagInfo[] flags = rc.senseNearbyFlags(-1, rc.getTeam().opponent());
-            if (flags.length > 0) {
-                Motion.bug2(flags[0].getLocation());
-            }
-            else {
-                MapLocation[] hiddenFlags = rc.senseBroadcastFlagLocations();
-                if (hiddenFlags.length > 0) {
-                    Motion.bug2(hiddenFlags[0]);
-                }
-                else {
-                    Motion.moveRandomly();
-                }
-            }
-        }
-        Attack.attack();
-        Attack.heal();
+        // oof nothing here
     }
 }
