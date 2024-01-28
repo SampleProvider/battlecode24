45301e4c7505e0b0030350d22691978b3ef9f466
Suvanth-Erranki <63676047+Suvanth-Erranki@users.noreply.github.com>
2024-01-28 17:01:53 -0500

    Macro and Flag Upgrades

---
+++ runmatches3.py
@@ -0,0 +1,172 @@
+from itertools import product
+import re
+import subprocess
+
+emojiMode = True
+emojiMap = {
+    'Won': ':white_check_mark:',
+    'Lost': ':x:',
+    'Tied': ':neutral_face:',
+    'N/A': ':black_large_square:',
+    'Error': ':exclamation:'
+}
+errors = []
+
+currentBot = 'COMBICAL' #bot to test
+
+bots = ['COMBIC', 'SPAARK'] #other bots
+
+maps = []
+
+# #Micro testing
+maps.append("[edge] microsmall")
+maps.append("[edge] micromedium")
+maps.append("[edge] microlarge")
+
+# #Default
+maps.append("DefaultSmall")
+maps.append("DefaultMedium")
+maps.append("DefaultLarge")
+maps.append("DefaultHuge")
+
+# #Sprint 1
+maps.append("AceOfSpades")
+maps.append("Alien")
+maps.append("Ambush")
+maps.append("Battlecode24")
+maps.append("BigDucksBigPond")
+maps.append("Canals")
+maps.append("CH3353C4K3F4CT0RY")
+maps.append("Duck")
+maps.append("Fountain")
+maps.append("Hockey")
+maps.append("HungerGames")
+maps.append("MazeRunner")
+maps.append("Rivers")
+maps.append("Snake")
+maps.append("Soccer")
+maps.append("SteamboatMickey")
+maps.append("Yinyang")
+
+#Sprint 2
+maps.append("BedWars")
+maps.append("Bunkers")
+maps.append("Checkered")
+maps.append("Diagonal")
+maps.append("Divergent")
+maps.append("EndAround")
+maps.append("FloodGates")
+maps.append("Foxes")
+maps.append("Fusbol")
+maps.append("GaltonBoard")
+maps.append("HeMustBeFreed")
+maps.append("Intercontinental")
+maps.append("Klein")
+maps.append("QueenOfHearts")
+maps.append("QuestionableChess")
+maps.append("Racetrack")
+maps.append("Rainbow")
+maps.append("TreeSearch")
+
+matches = list(product(bots, maps))
+
+numWinsMapping = {
+    0: 'Lost',
+    1: 'Tied',
+    2: 'Won',
+}
+
+def retrieveGameLength(output):
+    startIndex = output.find('wins (round ')
+    if startIndex == -1:
+        return -1
+    endIndex = output.find(')', startIndex)
+    if endIndex == -1:
+        return -1
+    return output[startIndex + len('wins(round ') + 1:endIndex]
+
+def run_match(bot, map):
+    try:
+        # outputA = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + currentBot, '-PteamB=' + bot, '-Pmaps=' + map]))
+        # outputB = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + bot, '-PteamB=' + currentBot, '-Pmaps=' + map]))
+        # for local windows testing
+        outputA = str(subprocess.check_output(['gradlew', 'run', '-PteamA=' + currentBot, '-PteamB=' + bot, '-Pmaps=' + map], shell=True))
+        outputB = str(subprocess.check_output(['gradlew', 'run', '-PteamA=' + bot, '-PteamB=' + currentBot, '-Pmaps=' + map], shell=True))
+    except subprocess.CalledProcessError as exc:
+        print("Status: FAIL", exc.returncode, exc.output)
+        return 'Error'
+    else:
+        winAString = '{} (A) wins'.format(currentBot)
+        winBString = '{} (B) wins'.format(currentBot)
+        loseAString = '{} (B) wins'.format(bot)
+        loseBString = '{} (A) wins'.format(bot)
+        resignedString = 'resigned'
+        
+        numWins = 0
+        
+        gameLengthA = retrieveGameLength(outputA)
+        gameAResigned = resignedString in outputA
+        gameLengthB = retrieveGameLength(outputB)
+        gameBResigned = resignedString in outputB
+
+        flagRegex = "FLAG{[^{}]*}"
+        gameAFlags = list(set(re.findall(flagRegex, outputA)))
+        gameBFlags = list(set(re.findall(flagRegex, outputB)))
+        gameAFlags = ", ".join([s[5:-1] for s in gameAFlags])
+        gameBFlags = ", ".join([s[5:-1] for s in gameBFlags])
+        if len(gameAFlags) > 0:
+            gameAFlags = "{{{}}}".format(gameAFlags)
+        if len(gameBFlags) > 0:
+            gameBFlags = "{{{}}}".format(gameBFlags)
+
+        gameAInfo = gameLengthA + ('*' if gameAResigned else '') + gameAFlags
+        gameBInfo = gameLengthB + ('*' if gameBResigned else '') + gameBFlags
+        
+        if winAString in outputA:
+            numWins += 1
+        else:
+            if not loseAString in outputA:
+                return 'Error'
+        if winBString in outputB:
+            numWins += 1
+        else:
+            if not loseBString in outputB:
+                return 'Error'
+        return (numWinsMapping[numWins] + ' (' + ', '.join([gameAInfo, gameBInfo]) + ')', numWins)
+
+results = {}
+ctr = 0
+#run matches
+for i in range(len(bots)):
+    bot = bots[i]
+    winsThisBot = 0
+    for j in range(len(maps)):
+        map = maps[j]
+        ctr = ctr + 1
+        print("#" + str(ctr) + " of " + str(len(bots)*len(maps)) + ": {} vs {} on {}".format(currentBot, bot, map))
+        results[(bot, map)], wins = run_match(bot, map)
+        winsThisBot += wins
+    print(currentBot + " won " + str(winsThisBot) + " of " + str(len(maps)*2) + " against " + bot + "\n")
+
+# Construct table
+table = [[results.get((bot, map), 'N/A') for bot in bots] for map in maps]
+
+def replaceWithDictionary(s, mapping):
+    for a, b in mapping.items():
+        s = s.replace(a, b)
+    return s
+
+if emojiMode:
+    table = [[replaceWithDictionary(item, emojiMap) for item in row] for row in table]
+
+# Write to file
+with open('matches-summary.txt', 'w') as f:
+    table = [[currentBot + " vs "] + bots, [':---:' for i in range(len(bots) + 1)]] + [[map] + row for map, row in zip(maps, table)]
+    for line in table:
+        f.write('| ')
+        f.write(' | '.join(line))
+        f.write(' |')
+        f.write('\n')
+    f.write('\n')
+    for error in errors:
+        f.write(error)
---
+++ src/COMBIC/RobotPlayer.java
@@ -0,0 +1,188 @@
+package COMBIC;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public strictfp class RobotPlayer {
+    protected static int turnCount = 0;
+
+    protected static Random rng;
+
+    protected static Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
+
+    protected static int mode = -1;
+    protected static MapLocation spawnLoc = new MapLocation(-1, -1);
+
+    protected final static int DEFENSIVE = 0;
+    protected final static int OFFENSIVE = 1;
+    protected final static int SCOUT = 2;
+
+    public static void run(RobotController rc) throws GameActionException {
+        rng = new Random(rc.getID() + 2024);
+        Motion.rc = rc;
+        Motion.rng = rng;
+        Atk.rc = rc;
+        Comms.rc = rc;
+        Setup.rc = rc;
+        Setup.rng = rng;
+        Offense.rc = rc;
+        Offense.rng = rng;
+        Defense.rc = rc;
+        Defense.rng = rng;
+        Scout.rc = rc;
+        Scout.rng = rng;
+
+        Comms.init();
+
+        int mapSizeFactor = (rc.getMapHeight() + rc.getMapWidth()) / 20 - 2;
+        int defensiveFactor = 3;
+        if (Comms.getFlagAdv() > 0) {
+            defensiveFactor += (Comms.getFlagAdv()) * 2 + rc.getRoundNum() / 600;
+        }
+        if (Comms.id < defensiveFactor) {
+            mode = DEFENSIVE;
+        }
+        else if (Comms.id < mapSizeFactor + defensiveFactor) {
+            //vary # of scouts based on map size
+            mode = SCOUT;
+        }
+
+        Clock.yield();
+
+        while (true) {
+            turnCount += 1;
+
+            try {
+                spawn: if (!rc.isSpawned()) {
+                    MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+                    MapLocation[] hiddenFlags = rc.senseBroadcastFlagLocations();
+                    if (mode == DEFENSIVE) {
+                        if (Comms.id < 6) {
+                            if (!Comms.hasLocation(rc.readSharedArray(Comms.ALLY_FLAG_DEF_LOC + (Comms.id % 3)))) {
+                                break spawn; // labels moment
+                            }
+                            for (int i = 27; --i >= 0;) {
+                                if (!rc.canSpawn(spawnLocs[i])) {
+                                    spawnLocs[i] = new MapLocation(-1000, -1000);
+                                }
+                            }
+                            MapLocation bestSpawnLoc = Motion.getClosest(spawnLocs, Comms.parseLocation(rc.readSharedArray(Comms.ALLY_FLAG_DEF_LOC + Comms.id)));
+                            if (bestSpawnLoc != null && rc.canSpawn(bestSpawnLoc)) {
+                                rc.spawn(bestSpawnLoc);
+                                break spawn;
+                            }
+                        }
+                    }
+                    if (hiddenFlags.length == 0 || rc.getRoundNum() <= 20) {
+                        int index = rng.nextInt(27 * 3);
+                        for (int i = 27; --i >= 0;) {
+                            MapLocation randomLoc = spawnLocs[index % spawnLocs.length];
+                            if (rc.canSpawn(randomLoc)) {
+                                rc.spawn(randomLoc);
+                                break spawn;
+                            }
+                            else {
+                                index++;
+                            }
+                        }
+                    }
+                    else {
+                        for (int i = 27; --i >= 0;) {
+                            if (!rc.canSpawn(spawnLocs[i])) {
+                                spawnLocs[i] = new MapLocation(-1000, -1000);
+                            }
+                        }
+                        MapLocation bestSpawnLoc = Motion.getClosestPair(spawnLocs, hiddenFlags);
+                        for (int i = 3; --i >= 0;) {
+                            if (Comms.isFlagInDanger(rc.readSharedArray(Comms.ALLY_FLAG_CUR_LOC + i))) {
+                                bestSpawnLoc = Motion.getClosest(spawnLocs, Comms.parseLocation(rc.readSharedArray(Comms.ALLY_FLAG_CUR_LOC + i)));
+                            }
+                        }
+                        // MapLocation bestSpawnLoc = Motion.getClosest(spawnLocs, Comms.parseLocation(rc.readSharedArray(Comms.ALLY_FLAG_DEF_LOC + Comms.id)));
+                        if (bestSpawnLoc != null && rc.canSpawn(bestSpawnLoc)) {
+                            rc.spawn(bestSpawnLoc);
+                        }
+                    }
+                }
+                StringBuilder indicatorString = new StringBuilder();
+                Motion.indicatorString = indicatorString;
+                Atk.indicatorString = indicatorString;
+                Comms.indicatorString = indicatorString;
+                Setup.indicatorString = indicatorString;
+                Offense.indicatorString = indicatorString;
+                Defense.indicatorString = indicatorString;
+                Scout.indicatorString = indicatorString;
+                if (!rc.isSpawned()) {
+                    if (rc.getRoundNum() < GameConstants.SETUP_ROUNDS) {
+                        Setup.jailed();
+                    }
+                    else if (mode == DEFENSIVE) {
+                        Defense.jailed();
+                    }
+                    else if (mode == SCOUT) {
+                        Scout.jailed();
+                    }
+                    else {
+                        Offense.jailed();
+                    }
+                }
+                else {
+                    Motion.opponentRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+                    Motion.friendlyRobots = rc.senseNearbyRobots(-1, rc.getTeam());
+                    Motion.flags = rc.senseNearbyFlags(-1);
+                    
+                    int attackPriority = -1 * Comms.getFlagAdv() + (rc.canBuyGlobal(GlobalUpgrade.ATTACK) ? 0 : -9999);
+                    int healPriority = Comms.getFlagAdv() + (rc.canBuyGlobal(GlobalUpgrade.HEALING) ? 0 : -9999);
+                    int capturePriority = (attackPriority < 0 && healPriority < 0 && rc.canBuyGlobal(GlobalUpgrade.CAPTURING)) ? 0 : -1000000;
+                    if (rc.canBuyGlobal(GlobalUpgrade.ATTACK) && attackPriority > 0 && attackPriority >= healPriority) {
+                        rc.buyGlobal(GlobalUpgrade.ATTACK);
+                    }
+                    if (rc.canBuyGlobal(GlobalUpgrade.HEALING) && healPriority > 0 && healPriority > attackPriority) {
+                        rc.buyGlobal(GlobalUpgrade.HEALING);
+                    }
+                    if (capturePriority > 0) {
+                        rc.buyGlobal(GlobalUpgrade.CAPTURING);
+                    }
+                    
+                    if (rc.getRoundNum() < GameConstants.SETUP_ROUNDS) {
+                        Setup.run();
+                    }
+                    else if (mode == DEFENSIVE) {
+                        Defense.run();
+                    }
+                    else if (mode == SCOUT) {
+                        Scout.run();
+                    }
+                    else {
+                        Offense.run();
+                    }
+                }
+                rc.setIndicatorString(indicatorString.toString());
+
+            }
+            catch (GameActionException e) {
+                System.out.println("GameActionException");
+                e.printStackTrace();
+                // rc.resign();
+            }
+            catch (Exception e) {
+                System.out.println("Exception");
+                e.printStackTrace();
+                // rc.resign();
+            }
+            finally {
+                Clock.yield();
+            }
+        }
+    }
+}
